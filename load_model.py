d01db6cedb3aae4d85ae7134ad1682ffe94a6d05
pinglend <wes@getpinglend.com>
2023-02-04 19:01:38 -0800

    WIP: load model

---
+++ load_model.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+
+model = tf.keras.models.load_model(
+    "model.h5",
+    # custom_objects={
+    #     "PositionalEncoding": PositionalEncoding,
+    #     "MultiHeadAttentionLayer": MultiHeadAttentionLayer,
+    # },
+    compile=False,
+)
