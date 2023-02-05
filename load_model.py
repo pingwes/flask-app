778a14219d54e8ead70c40a781be103bc509c9a7
pinglend <wes@getpinglend.com>
2023-02-04 19:08:02 -0800

    Positional encoding and multihead attention

--- load_model.py
+++ load_model.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 
 model = tf.keras.models.load_model(
     "model.h5",
-    # custom_objects={
-    #     "PositionalEncoding": PositionalEncoding,
-    #     "MultiHeadAttentionLayer": MultiHeadAttentionLayer,
-    # },
+    custom_objects={
+        "PositionalEncoding": PositionalEncoding,
+        "MultiHeadAttentionLayer": MultiHeadAttentionLayer,
+    },
     compile=False,
 )
